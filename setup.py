72f564c159a11b89b77a542f4eb48548f47b5dba
cpprian <cpprian456@gmail.com>
2024-12-03 20:30:21 +0100

    fix flake8 suggestion

--- setup.py
+++ setup.py
@@ -3,7 +3,8 @@ from setuptools import setup, find_packages
 setup(
     name="lzss",
     version="0.1.0",
-    description="An implementation of lossless data compression algorithm inherited from LZ77",
+    description="""An implementation of lossless
+    data compression algorithm inherited from LZ77""",
     packages=find_packages(),
     entry_points={
         "console_scripts": [
